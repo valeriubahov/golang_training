49cf17ccc77186cac45d91b176590aed97d2869c
Valeriu <valeriu.bahov@redspace.com>
2023-06-19 16:41:57 -0300

    some interfaces imporovements

--- interfaces/main.go
+++ interfaces/main.go
@@ -13,20 +13,20 @@ func main() {
 	eb := englishBot{}
 	sb := spanishBot{}
 
-	printGreeting(eb)
-	printGreeting(sb)
+	printGreeting(&eb)
+	printGreeting(&sb)
 }
 
 func printGreeting(b bot) {
 	fmt.Println(b.getGreeting())
 }
 
-func (englishBot) getGreeting() string {
+func (*englishBot) getGreeting() string {
 	// VERY custom logic for generating an english greeting
 	return "Hello friend!"
 }
 
-func (spanishBot) getGreeting() string {
+func (*spanishBot) getGreeting() string {
 	// VERY custom logic different from the other getGreeting for the english bot
 	return "Hola amigo!"
 }
